4756e74eef52920249d5cedc7064ff97e7f5ddb6
yungsavkas <78705243+savkas30@users.noreply.github.com>
2022-07-02 12:46:50 +0300

    Add new components (#3)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { Home } from './page/home/Home';
+import { Login } from './page/login/Login';
+import { Register } from './page/register/Register';
+import { Score } from './page/score/Score';
+import { User } from './page/user/User';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<>home</>} />
-        <Route path='/login' element={<>login</>}/>
-        <Route path='/register' element={<>register</>}/>
-        <Route path='/user' element={<>user</>}/>
-        <Route path='/score' element={<>score</>}/>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/user' element={<User/>}/>
+        <Route path='/score' element={<Score/>}/>
       </Routes>
     </div>
   );
