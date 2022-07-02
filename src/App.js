56376f5680b57f72a8e83ebed28dcf9845800810
yungsavkas <78705243+savkas30@users.noreply.github.com>
2022-07-02 12:22:15 +0300

    add routing (#2)
    * add routing
    
    * edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    
+      <Routes>
+        <Route path='/' element={<>home</>} />
+        <Route path='/login' element={<>login</>}/>
+        <Route path='/register' element={<>register</>}/>
+        <Route path='/user' element={<>user</>}/>
+        <Route path='/score' element={<>score</>}/>
+      </Routes>
     </div>
   );
 }
